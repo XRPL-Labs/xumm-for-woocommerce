8552fe2c44396b4ab62c655c854ce5f4fc66a25d
Andrei Rosseti <andrei@facedigital.com.br>
2023-02-15 23:14:23 -0300

    Removed useless comments

--- xumm-for-woocommerce.php
+++ xumm-for-woocommerce.php
@@ -44,7 +44,6 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
  */
 define( 'XUMM_FOR_WOOCOMMERCE_VERSION', '1.0.0' );
 
